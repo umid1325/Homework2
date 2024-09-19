1d3a3339d695d9833de46e4d767d2bf9db1e7c34
u.mamasaliyev <u.mamasaliyev@newuu.uz>
2024-09-19 18:42:21 +0500

    done1

--- problem1.cpp
+++ problem1.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main() {
     long long int n;
     cin >> n;
-    if (n<INT_MAX) {
+    if (n<INT_MAX or n>(-1)*INT_MAX) {
         cout<<"YES"<<endl;
     } else {
         cout<<"NO"<<endl;
--- problem4.cpp
+++ problem4.cpp
@@ -7,7 +7,7 @@ int main() {
     cin>>n1>>n2>>n3>>n4;
     if (n1==n2==n3==n4) {
         cout<<3;
-    }else if(n1==n2==n3 or n1==n4==n2 or n3==n4==n2) {
+    }else if(n1==n2==n3 or n1==n4==n2 or n3==n4==n2 or n3==n1==n4) {
         cout<<2;
     }else if ((n1==n2 and n3==n4) or (n1==n3 and n2==n4) or (n2==n3 and n4==n1)) {
         cout<<2;

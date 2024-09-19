3a7d1dd7787f1d437e54e9cac3ecf2d8a31ea3b4
u.mamasaliyev <u.mamasaliyev@newuu.uz>
2024-09-19 18:59:30 +0500

    done3

--- problem4.cpp
+++ problem4.cpp
@@ -5,9 +5,15 @@ using namespace std;
 int main() {
     int n1,n2,n3,n4;
     cin>>n1>>n2>>n3>>n4;
-    if (n1==n2==n3==n4) {
+    if (n1==n2 and n2==n3 and n3==n4) {
         cout<<3;
-    }else if(n1==n2==n3 or n1==n4==n2 or n3==n4==n2 or n3==n1==n4) {
+    }else if((n1==n2 and n1==n3 and n2==n3)
+            or
+            (n1==n4 and n1==n2 and n2==n4)
+            or
+            (n3==n4 and n3==n2 and n2==n4)
+            or
+            (n3==n1 and n3==n4 and n1==n4)) {
         cout<<2;
     }else if ((n1==n2 and n3==n4) or (n1==n3 and n2==n4) or (n2==n3 and n4==n1)) {
         cout<<2;
